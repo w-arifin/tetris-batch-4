b69db932b107e5a18251fd026d287ef98d5c01ff
w-arifin <161658099+w-arifin@users.noreply.github.com>
2024-02-29 15:22:53 +0700

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
-st.title('Analisis Kepuasan Konsumen Franchise ABD (Ayam Bang Dava')
-st.write("Ini adalah aplikasi web sederhana menggunakan Streamlit.")
+def main():
+    # Header
+    st.title("Analisis Kepuasan Konsumen Franchise ABD (Ayam Bang Dava)")
+    st.write("Created by Wondy Arifin")
+    
+    # Isi
+    st.write("ABD merupakan kepanjangan dari Ayam Bang Dava merupakan restoran yang terkenal dengan penjualannya yaitu Ayam Geprek.")
+    
+    # Gambar
+    st.image("https://via.placeholder.com/300", caption="Ini adalah contoh gambar", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
