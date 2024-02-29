3382b258721fa1b2071f26141581cc82d331d5f0
w-arifin <161658099+w-arifin@users.noreply.github.com>
2024-02-29 14:20:42 +0700

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title('Aplikasi Streamlit Sederhana')
+st.title('Analisis Kepuasan Konsumen Franchise ABD (Ayam Bang Dava')
 st.write("Ini adalah aplikasi web sederhana menggunakan Streamlit.")
