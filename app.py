0bfa1a783327445091e565173a2c9cb46490f530
w-arifin <161658099+w-arifin@users.noreply.github.com>
2024-03-01 11:25:46 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
-ct1 = st.container()
-with ct1:
-    st.header('Analisis Harga Sewa Indekos di Yogyakarta 🏡')
-    st.header('Subkhan Rian Romadhon | TETRIS Program Batch III')
-st.markdown('\n')
+def main():
+    # Header
+    st.title("Analisis Kepuasan Konsumen Franchise ABD (Ayam Bang Dava)")
+    st.write("Created By Wondy Arifin")
+    
+    # Isi
+    st.write("Di Kota Bandung, olahan daging ayam sudah tak terhitung lagi banyaknya. Setiap sudut kota, Anda begitu mudah menemui berbagai macam tempat makan yang menawarkan menu tersebut. Satu di antaranya yang patut anda sambangi adalah Kedai Ayam Bang Dava. Lokasinya berada di Jalan Mataram No 2, Cihapit, Kota Bandung.")
+    
+    # Gambar
+    st.image("https://via.placeholder.com/300", caption="Ini adalah contoh gambar", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
