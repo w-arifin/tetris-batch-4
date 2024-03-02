f96a291f75fefcf1d20067e5c47b9d4c7943d147
w-arifin <161658099+w-arifin@users.noreply.github.com>
2024-03-02 12:32:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ review, menu, dan tanggal pemesanan. Karena ini melibatkan seluruh cabang, maka 
 satu kolom lokasi sebagai pembeda untuk masing-masing *outlet*.
 
 """)
-st.image('https://media.discordapp.net/attachments/593140987170390016/1213345865817784380/image.png?ex=65f52365&is=65e2ae65&hm=cfc49466181c8949a711cd36724e54439f079a57634c904e03ef5c67550f25df&=&format=webp')
+    st.image('https://media.discordapp.net/attachments/593140987170390016/1213345865817784380/image.png?ex=65f52365&is=65e2ae65&hm=cfc49466181c8949a711cd36724e54439f079a57634c904e03ef5c67550f25df&=&format=webp')
