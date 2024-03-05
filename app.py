b26347eee278b0cf55fc856a40260a70cfaa155d
w-arifin <161658099+w-arifin@users.noreply.github.com>
2024-03-05 13:56:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,12 @@ from PIL import Image
 # import dari google colab
 import pandas as pd
 
-df = pd.DataFrame(
-    {
-        "name": ["Roadmap", "Extras", "Issues"],
-        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
-        "stars": [random.randint(0, 1000) for _ in range(3)],
-        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
-    }
-)
-st.dataframe(
-    df,
-    column_config={
-        "name": "App name",
-        "stars": st.column_config.NumberColumn(
-            "Github Stars",
-            help="Number of stars on GitHub",
-            format="%d ⭐",
-        ),
-        "url": st.column_config.LinkColumn("App URL"),
-        "views_history": st.column_config.LineChartColumn(
-            "Views (past 30 days)", y_min=0, y_max=5000
-        ),
-    },
-    hide_index=True,
-)
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
+
+st.dataframe(df.style.highlight_max(axis=0))
 
 
 st.title("Analisis Kepuasan Konsumen Franchise ABD (Ayam Bang Dava)")
