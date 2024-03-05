aa4c358e59abde4d609bd3f9247880fe5d0ed773
w-arifin <161658099+w-arifin@users.noreply.github.com>
2024-03-05 12:16:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -121,3 +121,10 @@ Setelah nantinya digabungkan, maka akan ada perubahan kolom yakni:
 # COLAB: Import Dataset
 # ----------------------
 
+@st.cache_data()
+def load_data(url, sheet_name=None):
+    df = pd.read_excel(url,sheet_name=sheet_name)
+    return df
+
+kost = load_data('dataset/Ayam Bang Dava, Nasi Geprek _ Popcorn, Antapani - GoFood.xlsx')
+st.dataframe(kost, use_container_width=True)
